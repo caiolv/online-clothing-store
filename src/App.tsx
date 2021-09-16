2526a430f18627891f2b521ad2a611d8ed9f39ef
Caio Oliveira <caiolv@me.com>
2021-09-16 17:09:51 -0300

    add: basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
+
+import history from './services/history';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Router history={history}>
+      <Header />
+      <Routes />
+      <GlobalStyle />
+    </Router>
   );
 }
 
